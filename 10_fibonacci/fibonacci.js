0e69b265b78b7c41e1c6db9d4e06c40c54b20c9e
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-08 16:33:08 +0530

    add sol to fib exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,21 @@
 const fibonacci = function(num) {
-    let fib = 1;
     let prevFib = 1;
-    if (num === 1 || num === 2) {
+    let prevPrevFib = 1;
+    if(num < 0) {
+        return "OOPS";
+    }
+    if (num == 1 || num == 2) {
         return 1;
     } else {
-        
-        for (i = 1; i<= num; i++){
-            fib = fib + i;
+        for (i = 3; i <= num; i++){
+            fib = prevFib + prevPrevFib;
+            prevPrevFib = prevFib;
+            prevFib = fib;
         }
         return fib;
     }
 };
 
-console.log(fibonacci(3));
+// console.log(fibonacci(6));
 // Do not edit below this line
 module.exports = fibonacci;
