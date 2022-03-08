8e712e4b450f62cfe337901a8447dc4be309e0f7
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-08 17:02:54 +0530

    add incomplete code to findTheOldest ex

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    minYears = 0;
+    for (let person of people) {
+        let yearsLived = person.yearOfDeath - person.yearOfBirth
+        if (yearsLived > minYears) {
+            minYears = yearsLived;
+        }
+    }
 };
 
 // Do not edit below this line
