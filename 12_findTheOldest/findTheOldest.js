e28e61cbf7916c7fdaf75b7e8a85875524b448ab
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-03-08 17:05:25 +0530

    add a probable sol to the 12th ex

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,14 @@
 const findTheOldest = function(people) {
     minYears = 0;
+    targetPerson = null;
     for (let person of people) {
         let yearsLived = person.yearOfDeath - person.yearOfBirth
         if (yearsLived > minYears) {
             minYears = yearsLived;
+            targetPerson = person;
         }
     }
+    return person;
 };
 
 // Do not edit below this line
